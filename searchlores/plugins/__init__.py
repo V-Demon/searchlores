4e330b92692e50ba0cf9ba397eda066d2569405f
JinroOe <jinro.oersted@gmail.com>
2026-06-27 19:40:47 +0200

    Update __init__.py
    Qwen

--- searchlores/plugins/__init__.py
+++ searchlores/plugins/__init__.py
@@ -1,1 +1,10 @@
-# Plugins module
+from .entropy_analyzer import EntropyAnalyzer
+from .complexity_analyzer import ComplexityAnalyzer
+from .metrics_visualizer import MetricsVisualizer
+
+__all__ = [
+    # ... exports existants ...
+    'EntropyAnalyzer',
+    'ComplexityAnalyzer',
+    'MetricsVisualizer',
+]# Plugins module
